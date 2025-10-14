630dacf5d2840569206f875aab2baba7de03dfae
juanIBG <juansrangel89@gmail.com>
2025-10-14 09:32:53 -0500

    contactame mensaje funcional

--- src/components/Pages/ContactMe/ContactMe.jsx
+++ src/components/Pages/ContactMe/ContactMe.jsx
@@ -1,10 +1,29 @@
 import { Footer } from '../../Layouts/Footer/Footer'
 import { Header } from '../../Layouts/Header/Header'
 import { Background } from '../../UI/Background/Background'
+import emailjs from '@emailjs/browser';
+import { useEffect } from 'react'; // Asegúrate de importar useEffect
 
 import './ContactMe.css'
 
 export const ContactMe = () => {
+
+    useEffect(() => {
+        // Configura el User ID aquí una sola vez
+        emailjs.init('25IpPYD8l15Xx45uI'); 
+    }, []);
+
+    function enviarEmail(e){
+        e.preventDefault();
+        
+        // Ahora solo necesitas 3 argumentos
+        emailjs.sendForm('service_s9e6u0n', 'template_0z8kyfn', e.target)
+            .then(res => {
+                console.log("Email enviado:", res);
+            }, (error) => {
+                console.error("Error al enviar email:", error);
+            });
+    }
   return (
     <>
     {/* <Background> */}
@@ -13,7 +32,7 @@ export const ContactMe = () => {
       <section className="contact">
         <div className="formulario">
             <h2>Contact me</h2>
-            <form id="frm-email">
+            <form id="frm-email" onSubmit={enviarEmail}>
                 <div className="form-group">
                     <label htmlFor="nombre">Name:</label>
                 <input type="text" id="username" name="username" required minLength="2" maxLength="50" pattern="[a-zA-Z\s]+" placeholder="Enter your name"/>
